d7a7184728a2c8e58207f86310d3dd0ac86a207b
Pzzzzz <hello-cd.plus@hotmail.com>
2020-12-09 17:53:08 +0800

    update README.md

--- Pbot/plugins/preprocess.py
+++ Pbot/plugins/preprocess.py
@@ -8,4 +8,15 @@ async def pre(bot: Bot, event: Event, state: dict):
         event.current_arg_images = [
             seg.data["url"] for seg in event.message if seg.type == "image"
         ]
-
+'''
+    if event.detail_type != "group":
+        return
+    async with db.pool.acquire() as conn:
+        values = await conn.fetch(
+            "select * from mg where gid = {}".format(event.group_id)
+        )
+        if "on" == event.raw_message[:2] or "off" == event.raw_message[:3]:
+            return
+        if len(values) == 0:
+            raise CanceledException("该群不处于白名单中")
+'''
